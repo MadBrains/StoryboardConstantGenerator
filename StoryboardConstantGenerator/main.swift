8f24325183f04a55078024d84d4b11814098939b
i.ushakov <i.ushakov@perpetuumlab.com>
2016-02-25 19:12:38 +0300

    Initial Commit

---
+++ StoryboardConstantGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StoryboardConstantGenerator
+//
+//  Created by Иван Ушаков on 25.02.16.
+//  Copyright © 2016 Perpetuum Mobile lab. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
